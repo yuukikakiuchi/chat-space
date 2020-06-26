e5ca9c72daf10fbced81a65c07b4512674accc3b
yuukikakiuchi <ccb64890.yu@icloud.com>
2020-06-26 16:49:16 +0900

    replaced the message in　groupEdit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
+  root 'groups#index'
 end
