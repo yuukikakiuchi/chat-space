5d9caec24068285e94c32be02e201d6495c7fb71
yuukikakiuchi <ccb64890.yu@icloud.com>
2020-07-05 11:37:52 +0900

    replaced the message in SCSS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,6 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   include CarrierWave::MiniMagick
+  process resize_to_fit: [800, 800]
   
   # Choose what kind of storage to use for this uploader:
   storage :fog
@@ -31,7 +32,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  process resize_to_fit: [800, 800]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
